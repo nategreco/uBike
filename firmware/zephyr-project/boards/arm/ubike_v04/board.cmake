cc084fab27fc916a708ba0d35be9eb6e92ee5cdc
Nathan Greco <nathan.greco@gmail.com>
2024-01-02 18:11:27 -0700

    Created v0.4 board from v0.3 files

---
+++ firmware/zephyr-project/boards/arm/ubike_v04/board.cmake
@@ -0,0 +1,22 @@
+# Universal Bike Controller
+# Copyright (C) 2022-2023, Greco Engineering Solutions LLC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
